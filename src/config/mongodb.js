7372b1a45c9458cdbbb0f8c2cd3885b14908c63c
donlangdang <donlangdangcc@gmail.com>
2024-01-25 23:04:27 +0700

    add mongodb uri

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,9 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+// username: donlangdang
+//pass mongo: 5GxlK9ADwukZZ5It
+
+const MONGODB_URI = 'mongodb+srv://donlangdang:5GxlK9ADwukZZ5It@cluster0-donlangdang.ylhk9lf.mongodb.net/?retryWrites=true&w=majority'
+
+const DATABASE_NAME = 'trello-donlangdang-db'
