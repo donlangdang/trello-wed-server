a6491e163e6f52a2e2309f511fb63040cc959fc8
donlangdang <donlangdangcc@gmail.com>
2024-01-26 23:47:47 +0700

    ket noi, mo, dong mongodb

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,9 +3,38 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
-// username: donlangdang
-//pass mongo: 5GxlK9ADwukZZ5It
 
 const MONGODB_URI = 'mongodb+srv://donlangdang:5GxlK9ADwukZZ5It@cluster0-donlangdang.ylhk9lf.mongodb.net/?retryWrites=true&w=majority'
 
 const DATABASE_NAME = 'trello-donlangdang-db'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+// tạo ra đối tượng trelloDatabaseInstance bắt đâù là null(vì chưa connect)
+let trelloDatabaseInstance = null
+// khởi tạo đối tượng Client Instance (MongoClientInstance) để connect tới mongodb
+const MongoClientInstance = new MongoClient(MONGODB_URI, {
+  // lưu ý: serverAPI có từ phiên bản 5.0.0 trở lên, có thể không dùng nó, còn nêú dùng thì sẽ chỉ định 1 cái Stable API Version của mongoDB.
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+// kết nối Database
+export const CONNECT_DB = async () => {
+  // gọi kết nối tới mongodb Atlas với URI đã khai báo trong thân MongoClientInstance
+  await MongoClientInstance.connect()
+  // kết nối thành công thì lấy ra database theo tên và gán ngược lại nó lại vào biến trelloDatabaseInstance.
+  trelloDatabaseInstance = MongoClientInstance.db(DATABASE_NAME)
+}
+// Function GET_DB(không async) này có nhiệm vụ export ra trelloDatabaseInstance sau khi đã connect thành công tới mongodb để sử dụng nhiều nơi khác trong code.
+// lưu ý: phải đảm bảo chỉ luôn gọi GET_DB sau khi đã kết nối thành công
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first!')
+  return trelloDatabaseInstance
+}
+
+// đóng kết nối database khi cần
+export const CLOSE_DB = async () => {
+  await MongoClientInstance.close()
+}
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * Updated by trungquandev.com's author on August 17 2023
  * YouTube: https://youtube.com/@trungquandev
@@ -5,28 +6,37 @@
  */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
+import exitHook from 'async-exit-hook'
 
-const app = express()
+const STATR_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  const hostname = 'localhost'
+  const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', /*async*/ (req, res) => {
+    // console.log(await GET_DB().listCollections().toArray())
+    // Test Absolute import mapOrder
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Donlangdang, I am running at http://${hostname}:${port}/`)
-})
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello Donlangdang, I am running at http://${hostname}:${port}/`)
+  })
+  // thực hiện các tác vụ cleanup trước khi đóng server
+  exitHook(() => {
+    CLOSE_DB()
+  })
+}
+// chỉ khi kết nối với database thành công thì mới start server back-end lên.
+// sau này code sẽ đi tử STATR_SERVER đi ra và chia các file cuối cùng cũng gắn vào STATR_SERVER để chạy
+// học thêm try catch, IIFE javasript
+CONNECT_DB()
+  .then(() => console.log('Connected to MongoDB Cloud Atlas!'))
+  .then(() => STATR_SERVER())
+  .catch(error => {
+    console.error(error)
+    process.exit(0)
+  })
