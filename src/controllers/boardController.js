6dbd9a0ed51ec83f3690eb58ff8665b51972463a
donlangdang <donlangdangcc@gmail.com>
2024-01-31 22:45:06 +0700

    lam services

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -5,13 +5,22 @@
  */
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
+import { boardService } from '~/services/boardService'
 
 
 const createNew = async (req, res, next) => {
   try {
     // console.log('req.body: ', req.body)
+    // console.log('req.query: ', req.query)
+    // console.log('req.params: ', req.params)
+    // console.log('req.cookies: ', req.cookies)
+    // console.log('req.jwtDecoded: ', req.jwtDecoded)
 
-    res.status(StatusCodes.CREATED).json({ message: 'POST from controller API create new board' })
+    // điều hướng dữ liệu sang tầng Services
+    const createBoard = await boardService.createNew(req.body)
+
+    // có kết quả thì trả về client
+    res.status(StatusCodes.CREATED).json(createBoard)
   } catch (error) { next(error) }
 }
 
---
+++ src/services/boardService.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-useless-catch */
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+
+import { slugify } from '~/utils/formatters'
+
+// service thường dùng để xử lí logic các hàm các thứ như kiểu truy xuất dữ liệu trong database rồi chuyển sang cho controller để đẩy qua route chứ service ko chứa route hay điều hướng hoặc những gì tương tự.
+const createNew = async (reqBody) => {
+  try {
+    // xử lí logic dữ liệu đặc thù tùy dự án
+    const newBoard = {
+      ...reqBody,
+      // slug là gì ?
+      slug: slugify(reqBody.title)
+    }
+    // gọi tới tầng Model để xử lí lưu bản ghi newBoard vào trong Database
+    //...
+    // làm thêm các xử lí logic khác với các collection khác tùy đặc thù dự án...vv
+    // bắn Email, Notification về cho admin khi có 1 board mới đc tạo... v v
+    // trả kết quả về trong services luôn phải có return
+    return newBoard
+  } catch (error) { throw error }
+
+}
+
+export const boardService = {
+  createNew
+}
