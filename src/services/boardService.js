ba1b675c59b29df1cdedfd6724aa63fe63dc0a4e
donlangdang <donlangdangcc@gmail.com>
2024-02-02 23:36:25 +0700

    api create new board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -6,6 +6,7 @@
  */
 
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
 
 // service thường dùng để xử lí logic các hàm các thứ như kiểu truy xuất dữ liệu trong database rồi chuyển sang cho controller để đẩy qua route chứ service ko chứa route hay điều hướng hoặc những gì tương tự.
 const createNew = async (reqBody) => {
@@ -17,11 +18,16 @@ const createNew = async (reqBody) => {
       slug: slugify(reqBody.title)
     }
     // gọi tới tầng Model để xử lí lưu bản ghi newBoard vào trong Database
-    //...
+    const createdBoard = await boardModel.createNew(newBoard)
+    // console.log(createdBoard)
+
+    // lấy bản ghi board sau khi gọi (tuy thuộc dự án)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+    console.log(getNewBoard)
     // làm thêm các xử lí logic khác với các collection khác tùy đặc thù dự án...vv
     // bắn Email, Notification về cho admin khi có 1 board mới đc tạo... v v
     // trả kết quả về trong services luôn phải có return
-    return newBoard
+    return getNewBoard
   } catch (error) { throw error }
 
 }
