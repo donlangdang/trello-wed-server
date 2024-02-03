f0f678a5f913549c2386aa92a09068e5cb83652e
donlangdang <donlangdangcc@gmail.com>
2024-02-03 23:16:32 +0700

    hoc cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,8 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+// những domain đc truy cập vào server
+export const WHITELIST_DOMAINS = [
+  'http://localhost:5173'
+  //  sau này deploy lên domain nào khác thì gắn vào đây
+]
